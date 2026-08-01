9ba83185a8e24181bb2e837aec80a07fade7809e
abenlarbi482-creator <abenlarbi482@gmail.com>
2026-08-01 14:50:52 +0100

    Update eth_parser.vhd

--- eth_parser.vhd
+++ eth_parser.vhd
@@ -1,35 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 07.05.2026 13:00:11
--- Design Name: 
--- Module Name: eth_parser - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
+--	State machine that parses Ethernet, IPv4, and UDP headers, filters by EtherType/protocol/port, and extracts the UDP payload.
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity eth_parser is
    Port (clk_50: in std_logic;
