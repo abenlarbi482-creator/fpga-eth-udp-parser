b7950dde80064e0c5548fc9b9638f4fec5caa210
abenlarbi482-creator <abenlarbi482@gmail.com>
2026-08-01 14:49:16 +0100

    Update crc32.vhd

--- crc32.vhd
+++ crc32.vhd
@@ -1,35 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 09.05.2026 11:45:39
--- Design Name: 
--- Module Name: crc32 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
+--	Computes a running CRC32 over the frame (Ethernet CRC-32 polynomial) to verify frame integrity.
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity crc32 is
   port (
